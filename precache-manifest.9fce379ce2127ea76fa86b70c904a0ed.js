661fc7c9c5fc4bb14f588d42f7f1d1faf10569dc
楊崇遠 <tony@yangchongyuande-MacBook-Pro.local>
2020-05-20 23:08:04 +0800

    Updates

---
+++ precache-manifest.9fce379ce2127ea76fa86b70c904a0ed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8653a1795e30382185186b194e71685d",
+    "url": "/ReactCMS/index.html"
+  },
+  {
+    "revision": "5c5ca3b019dd87e51af2",
+    "url": "/ReactCMS/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c99754671b01ccdf9260",
+    "url": "/ReactCMS/static/css/main.4e0d1d3b.chunk.css"
+  },
+  {
+    "revision": "5c5ca3b019dd87e51af2",
+    "url": "/ReactCMS/static/js/2.fa6b98d2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ReactCMS/static/js/2.fa6b98d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c99754671b01ccdf9260",
+    "url": "/ReactCMS/static/js/main.0d33ff24.chunk.js"
+  },
+  {
+    "revision": "a6daddc3f6ebb7e2bfe6",
+    "url": "/ReactCMS/static/js/runtime-main.4a7e2e44.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactCMS/static/media/logo.5d5d9eef.svg"
+  }
+]);
